80defb78617df918d8c73e9c7f4e24a4e3f3dfa5
spell911 <spell911@hotmail.co.th>
2018-05-14 16:13:28 +0700

    route use

--- app.js
+++ app.js
@@ -1,35 +1,75 @@
-//createServer
-// http.createServer(function(req,res){ 
-// 	res.writeHead(200,{'Content-Type':'text/plain'});
-// 	res.end('am in!\n');
-// }).listen(8000);
-// console.log("server running port 8000"); 
+//require
+var express = require("express");
+const bodyParser = require("body-parser");
+const birds = require("./birds");
 
-var msg = "ggwp";
-//Module Pattern 1
-require('./foo.js');
-foo();
-foo2(msg);
-//Module Pattern 2
-var bar = require('./bar.js');
-bar();
-//Module Pattern 3
-var fiz = require('./fiz.js'); 
-fiz.fiz();
-fiz.fiz2(msg);
-//Module Pattern 4
-var buz = require('./buz.js'); 
-buz.log();
-buz.log2(msg);
-//Module Pattern 5
-var baz = require('./baz.js').Baz; 
-baz.log();
-//Module Pattern 6
-var Doo = require('./doo.js'); 
-var doo = new Doo();
-doo.log();
-//Module Pattern 7
-var Qux = require('./qux.js').Qux; 
-var qux = new Qux(); 
-qux.log();
+//express use
+var app = express();
+var server = require("http").Server(app);
+server.listen(8000);
+app.use(express.static("web"));
+console.log("server running port 8000");
 
+//bodyParser use
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+//birds use
+app.use("/birds", birds);
+
+/***************************************************/
+/********************* stuff ***********************/
+/***************************************************/
+
+//query string
+app.get("/jungle", function(req, res) {
+  console.log(req.query.userName);
+  res.end("Welcome to get..." + req.query.userName);
+});
+
+//segment
+app.get("/jungle/:userName", function(req, res) {
+  console.log(req.params);
+  res.end("Welcome to get..." + req.params.userName);
+});
+
+//json body-parser
+app.post("/desert", function(req, res) {
+  console.log(req.body);
+  let obj = { code: "cod", name: req.body.userName };
+  res.json(obj);
+  // res.send('Welcome to post...' + req.body.userName);
+});
+
+//Route handlers
+var cb0 = function(req, res, next) {
+  console.log("CB0");
+  next();
+};
+
+var cb1 = function(req, res, next) {
+  console.log("CB1");
+  next();
+};
+
+var cb2 = function(req, res) {
+  res.send("Hello from C!");
+};
+
+app.get("/example/c", [cb0, cb1, cb2]);
+
+//app Route
+// app.route('/book')
+//   .get(function(req, res) {
+//     res.send('Get a random book');
+//   })
+//   .post(function(req, res) {
+//     res.send('Add a book');
+//   })
+//   .put(function(req, res) {
+//     res.send('Update the book');
+//   });
